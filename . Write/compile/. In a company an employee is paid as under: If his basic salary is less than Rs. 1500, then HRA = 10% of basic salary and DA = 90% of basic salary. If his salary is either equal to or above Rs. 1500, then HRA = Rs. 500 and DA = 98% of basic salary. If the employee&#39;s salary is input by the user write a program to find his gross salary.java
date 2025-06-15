44d5008fb091cff2acd6a5a8f1c14d6d9250599e
Payal badgariya <payalbadgariya@gmail.com>
2025-06-15 14:46:59 +0530

    Create . In a company an employee is paid as under: If his basic salary is less than Rs. 1500, then HRA = 10% of basic salary and DA = 90% of basic salary. If his salary is either equal to or above Rs. 1500, then HRA = Rs. 500 and DA = 98% of basic salary. If the employee&#39;s salary is input by the user write a program to find his gross salary.java

---
+++ . Write/compile/. In a company an employee is paid as under: If his basic salary is less than Rs. 1500, then HRA = 10% of basic salary and DA = 90% of basic salary. If his salary is either equal to or above Rs. 1500, then HRA = Rs. 500 and DA = 98% of basic salary. If the employee&#39;s salary is input by the user write a program to find his gross salary.java
@@ -0,0 +1,31 @@
+//. In a company an employee is paid as under:
+If his basic salary is less than Rs. 1500, then HRA = 10% of basic salary and DA =
+90% of basic
+salary. If his salary is either equal to or above Rs. 1500, then HRA = Rs. 500 and DA =
+98% of
+basic salary. If the employee&#39;s salary is input by the user write a program to find his
+gross salary.
+//
+
+import java.util.Scanner;
+
+public class GrossSalaryCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the basic salary of the employee: ");
+        double basicSalary = scanner.nextDouble();
+        double HRA, DA, grossSalary;
+
+        if (basicSalary < 1500) {
+            HRA = 0.1 * basicSalary;
+            DA = 0.9 * basicSalary;
+        } else {
+            HRA = 500;
+            DA = 0.98 * basicSalary;
+        }
+
+        grossSalary = basicSalary + HRA + DA;
+
+        System.out.println("Gross Salary: Rs. " + grossSalary);
+    }
+}
